d702caaa382dfeffa1e02043ec8a8618eff8062d
Nick Hamlin <nickhamlin@gmail.com>
2020-06-07 19:58:54 -0400

    add theming to local repo to allow GHA builds

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "../pelican-themes/simple-bootstrap"
+THEME = "./theme"
 
 MARKUP = ('md') #, 'ipynb')
 
